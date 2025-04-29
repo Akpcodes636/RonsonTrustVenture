0bc5aac72cf5e5112c40f5c56a69074075f0517e
Ewherhe Akpesiri <ewherheakpesiri@gmail.com>
2025-04-29 17:30:29 +0100

    fix: update import paths to use lowercase for Home components

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 "use client";
 import Footer from "./components/footer";
-import About from "./components/Home/About";
-import FeaturedProducts from "./components/Home/FeaturedProduct";
-import Hero from "./components/Home/Hero";
-import Testimonial from "./components/Home/Testimonial";
-import WhyChooseUs from "./components/Home/WhyChooseUs";
+import About from "./components/home/About";
+
+import FeaturedProducts from "./components/home/FeaturedProduct";
+import Hero from "./components/home/Hero";
+import Testimonial from "./components/home/Testimonial";
+import WhyChooseUs from "./components/home/WhyChooseUs";
 
 export default function Home() {
   return (
